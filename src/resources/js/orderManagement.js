3ba6228d8eb84418521903d93340c18c2a64a4cf
Kiril <hello@nonamez.name>
2022-03-15 19:28:59 +0200

    fix

--- src/resources/js/orderManagement.js
+++ src/resources/js/orderManagement.js
@@ -4,27 +4,29 @@ const bootstrap = require('bootstrap');
 
 const divOrderManagementModal = document.querySelector('#orderManagement');
 
-const orderManagementModal = new bootstrap.Modal(divOrderManagementModal, {
-	keyboard: false
-});
+if (divOrderManagementModal) {
+	const orderManagementModal = new bootstrap.Modal(divOrderManagementModal, {
+		keyboard: false
+	});
 
-// orderManagementModal.show();
+	// orderManagementModal.show();
 
-document.querySelector('#orderSave').addEventListener('click', async function() {
-	let data = {};
+	document.querySelector('#orderSave').addEventListener('click', async function() {
+		let data = {};
 
-	// [...orderManagementModal.querySelectorAll('[name]')].forEach(e => data[e.name] = e.value);
-	divOrderManagementModal.querySelectorAll('[name]').forEach(e => data[e.name] = e.value);
+		// [...orderManagementModal.querySelectorAll('[name]')].forEach(e => data[e.name] = e.value);
+		divOrderManagementModal.querySelectorAll('[name]').forEach(e => data[e.name] = e.value);
 
-	let response = await axios.post('/orders/save', data);
+		let response = await axios.post('/orders/save', data);
 
-	if (response) {
-		toastr.success('ORDER CREATED!');
+		if (response) {
+			toastr.success('ORDER CREATED!');
 
-		orderManagementModal.hide();
+			orderManagementModal.hide();
 
-		setTimeout(() => {
-			window.location.reload();
-		}, 1000);
-	}
-});
+			setTimeout(() => {
+				window.location.reload();
+			}, 1000);
+		}
+	});
+}
